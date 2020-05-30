c71d0e29cd6a08bd2a2c9d7dd5867f1356ccdc48
Rediet8abere <redietaberegesesse@gmail.com>
2020-05-29 20:54:36 -0700

    Added 404 status

--- location.go
+++ location.go
@@ -15,6 +15,9 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
     } else if r.URL.Path == "/contact" {
       fmt.Fprintf(w, "To get in touch, please send an eamil to <a href=\"mailto:support@lenslocked.com\"> support@lenslocked.com</a>.")
+    } else {
+      w.WriteHeader(http.StatusNotFound)
+      fmt.Fprint(w, "<h1>Invalid Page</h1>")
     }
 
 
