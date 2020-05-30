e7b530c01b8ab4d45c8087332d60194834a5b574
Rediet8abere <redietaberegesesse@gmail.com>
2020-05-29 20:37:48 -0700

    added a contact page

--- location.go
+++ location.go
@@ -7,9 +7,18 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "<h1>Hi there, Curious where you are, me too!</h1>")
-    fmt.Fprintf(w, "You are at %s!\n", r.URL.Path[1:])
-    fmt.Fprintf(w, "Let's see what you know about %s", r.URL.Path[1:])
+    w.Header().Set("Content-Type", "text/html")
+    if r.URL.Path == "/" {
+      fmt.Fprintf(w, "<h1>Hi there, Curious where you are, me too!</h1>")
+      fmt.Fprintf(w, "You are at %s!\n", r.URL.Path[1:])
+      fmt.Fprintf(w, "Let's see what you know about %s", r.URL.Path[1:])
+
+    } else if r.URL.Path == "/contact" {
+      fmt.Fprintf(w, "To get in touch, please send an eamil to <a href=\"mailto:support@lenslocked.com\"> support@lenslocked.com</a>.")
+    }
+
+
+
 }
 
 func main() {
