58c8a3decf9fdafea44a456829333cae35435ef1
Rediet8abere <redietaberegesesse@gmail.com>
2020-06-09 14:27:42 -0700

    switching

--- pullDataFromInternet.go
+++ pullDataFromInternet.go
@@ -4,15 +4,27 @@ import (
   "fmt"
   "net/http"
   "io/ioutil"
+  "encoding/xml"
 )
 
+type SitemapIndex struct {
+  Locations []Location `xml:"sitemap"`
+}
 
+type Location struct {
+  Loc string `xml:"loc"`
+}
 
+func (L Location) String() string {
+  return fmt.Sprintf(L.Loc)
+}
 func main() {
   // sitemaps that conatian links to all catogrize site maps
   resp, _ := http.Get("https://www.washingtonpost.com/sitemaps/index.xml")
   bytes, _ := ioutil.ReadAll(resp.Body)
-  string_body := string(bytes)
-  fmt.Println(string_body)
   resp.Body.Close()
+
+  var s SitemapIndex
+  xml.Unmarshal(bytes, &s)
+  fmt.Println(s.Locations)
 }
