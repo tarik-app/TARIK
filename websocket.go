5254459238c878650ba52e3ee7ad1cb6c2be334f
Rediet8abere <redietaberegesesse@gmail.com>
2020-07-09 09:59:21 -0700

    websocket to http

--- websocket.go
+++ websocket.go
@@ -5,74 +5,77 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
-
-	"github.com/gorilla/websocket"
-	"github.com/tarik-app/TARIK/touristmedia"
 )
 
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-}
+// var upgrader = websocket.Upgrader{
+// 	ReadBufferSize:  1024,
+// 	WriteBufferSize: 1024,
+// }
 
 type LatitLong struct {
 	Latit float64 `json:"latit"`
 	Longi float64 `json:"longi"`
 }
 
-func reader(conn *websocket.Conn) {
-	// listening for incoming messages
-	for {
-		messageType, p, err := conn.ReadMessage()
-		if err != nil {
-			log.Println(err)
-			return
-		}
+// func reader(conn *websocket.Conn) {
+// 	// listening for incoming messages
+// 	for {
+// 		messageType, p, err := conn.ReadMessage()
+// 		if err != nil {
+// 			log.Println(err)
+// 			return
+// 		}
 
-		if err := conn.WriteMessage(messageType, p); err != nil {
-			log.Println(err)
-			return
-		}
+// 		if err := conn.WriteMessage(messageType, p); err != nil {
+// 			log.Println(err)
+// 			return
+// 		}
 
-		// getting long and latit as json data
-		var coor LatitLong
-		// storing it in struct
-		err = json.Unmarshal([]byte(p), &coor)
-		if err != nil {
-			fmt.Println(err)
-		}
-		mediasummeryresult := touristmedia.GetNearbyTouristAttraction(coor.Latit, coor.Longi)
-		fmt.Println(mediasummeryresult)
+// 		// getting long and latit as json data
+// 		var coor LatitLong
+// 		// storing it in struct
+// 		err = json.Unmarshal([]byte(p), &coor)
+// 		if err != nil {
+// 			fmt.Println(err)
+// 		}
+// 		mediasummeryresult := touristmedia.GetNearbyTouristAttraction(coor.Latit, coor.Longi)
+// 		fmt.Println(mediasummeryresult)
 
-		if err := conn.WriteJSON(mediasummeryresult); err != nil {
-			log.Println(err)
-			break
-		}
+// 		if err := conn.WriteJSON(mediasummeryresult); err != nil {
+// 			log.Println(err)
+// 			break
+// 		}
 
-	}
-}
+// 	}
+// }
 
-func wsEndpoint(w http.ResponseWriter, r *http.Request) {
-	// upgrade an incoming connection
-	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+// func wsEndpoint(w http.ResponseWriter, r *http.Request) {
+// 	// upgrade an incoming connection
+// 	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
 
-	fmt.Println(r.Host)
+// 	fmt.Println(r.Host)
 
-	ws, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Println(err)
-	}
-	log.Println("Client Successfully connected.....")
-	reader(ws)
+// 	ws, err := upgrader.Upgrade(w, r, nil)
+// 	if err != nil {
+// 		log.Println(err)
+// 	}
+// 	log.Println("Client Successfully connected.....")
+// 	reader(ws)
+// }
+
+func Location(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	var coor LatitLong
+	_ = json.NewDecoder(r.Body).Decode(&coor)
+	fmt.Println(coor, r.Body)
+	json.NewEncoder(w).Encode(coor)
 }
 
 func main() {
 	fmt.Println("Go WebSocket!")
-	http.HandleFunc("/ws", wsEndpoint)
-	port := os.Getenv("PORT")
-	http.ListenAndServe(":"+port, nil)
-	// log.Fatal(http.ListenAndServe(os.Getenv("PORT"), nil))
-	// .listen(process.env.PORT || 5000)
-	// ":8080"
+	http.HandleFunc("/location", Location)
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
