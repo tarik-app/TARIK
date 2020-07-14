a19a7be2ba1ed6c6f89ea02dfd23df631be639dd
Rediet8abere <redietaberegesesse@gmail.com>
2020-07-13 17:54:31 -0700

    / to location

--- websocket.go
+++ websocket.go
@@ -35,7 +35,7 @@ func main() {
 	fmt.Println(port)
 	fmt.Println("Go WebSocket!")
 
-	r.HandleFunc("/", Location)
+	r.HandleFunc("/location", Location)
 
 	log.Fatal(http.ListenAndServe(port, r))
 	// ":8000"
